3a0f450d0ba4b0efd0692f37a32f902e40015b05
root <root@d9620cce76b0>
2020-03-29 07:34:56 +0000

    add fetchmethod

---
+++ sample/betterhome/build.sh
@@ -0,0 +1,6 @@
+scrapy crawl fetchmethod -a category=煮る
+scrapy crawl fetchmethod -a category=揚げる
+scrapy crawl fetchmethod -a category=蒸す
+scrapy crawl fetchmethod -a category=焼く
+scrapy crawl fetchmethod -a category=炒める
+scrapy crawl fetchmethod -a category=その他
